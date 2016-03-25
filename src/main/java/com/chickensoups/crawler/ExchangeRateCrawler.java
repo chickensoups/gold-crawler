b1ee499f423bb648c7f24f88b3e5f7ef6b03fd24
Chickensoups <vuongkm01526@gmail.com>
2016-03-25 17:33:17 +0700

    change paclage name to crawler, add exchange reate crawler class, add datautil class

---
+++ src/main/java/com/chickensoups/crawler/ExchangeRateCrawler.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chickensoups.crawler;
+
+/**
+ *
+ * @author ai02
+ */
+public class ExchangeRateCrawler {
+
+    Site vietcombank = new Site("http://www.vietcombank.com.vn/exchangerates/?lang=en");
+    Site vietinbank = new Site("https://www.vietinbank.vn/web/home/vn/ty-gia/");
+    Site tpbank = new Site("https://tpb.vn/financial-toolkit/ty-gia-ngoai-te");
+    Site msb = new Site("https://www.msb.com.vn");
+    Site vpbank = new Site("http://www.vpbank.com.vn/vpb-exchange-rates");
+    Site sacombank = new Site("http://www.sacombank.com.vn/Pages/default.aspx");
+    Site scb = new Site("https://www.scb.com.vn/exchangerate.aspx");
+    Site eximbank = new Site("https://www.eximbank.com.vn/home/Static/Exchangerate.aspx");
+    Site shb = new Site("http://www.shb.com.vn/tabid/465/default.aspx");
+    
+    
+}
